75d93d1b85942ef2e2f741380725406b6455558f
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-10-22 23:41:04 +0900

    1022

--- Pr6_3/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
+++ Pr6_3/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
@@ -1,14 +1,31 @@
 package kr.ac.hs.and2021.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpecSong = tabHost.newTabSpec("SONG").setIndicator("음악별");
+        tabSpecSong.setContent(R.id.tabSong);
+        tabHost.addTab(tabSpecSong);
+
+        TabHost.TabSpec tabSpecArtist = tabHost.newTabSpec("ARTIST").setIndicator("가수별");
+        tabSpecArtist.setContent(R.id.tabArtist);
+        tabHost.addTab(tabSpecArtist);
+
+        TabHost.TabSpec tabSpecAlbum = tabHost.newTabSpec("ALBUM").setIndicator("앨범별");
+        tabSpecAlbum.setContent(R.id.tabAlbum);
+        tabHost.addTab(tabSpecAlbum);
+
+        tabHost.setCurrentTab(0);
     }
 }
