f7c181e640c33ecfecab08cb9dd39663921194a9
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-10-22 21:50:48 +0900

    1022

--- Pr6_1/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
+++ Pr6_1/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
@@ -1,14 +1,107 @@
 package kr.ac.hs.and2021.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Calendar;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Chronometer chrono;
+        Button btnStart, btnEnd;
+        RadioButton rdoCal, rdoTime;
+        CalendarView calView;
+        TimePicker tPicker;
+        TextView tvYear, tvMonth, tvDay, tvHour, tvMinute;
+        int selectYear, selectMonth, selectDay;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        setTitle("시간예약");
+        btnStart = (Button)findViewById(R.id.btnStart);
+        btnEnd = (Button)findViewById(R.id.btnEnd);
+
+        chrono = (Chronometer) findViewById(R.id.chronometer1);
+        rdoCal = (RadioButton) findViewById(R.id.rdoCal);
+        rdoTime = (RadioButton) findViewById(R.id.rdoTime);
+
+        // FrameLayout의 2개 위젯
+        tPicker = (TimePicker) findViewById(R.id.timePicker1);
+        calView = (CalendarView) findViewById(R.id.calendarView1);
+
+        // 텍스트뷰 중에서 연, 월, 일, 시, 분 숫자
+        tvYear = (TextView) findViewById(R.id.tvYear);
+        tvMonth = (TextView) findViewById(R.id.tvMonth);
+        tvDay = (TextView) findViewById(R.id.tvDay);
+        tvHour =(TextView) findViewById(R.id.tvHour);
+        tvMinute = (TextView) findViewById(R.id.tvMinute);
+
+        //처음에는 2개를 안보이게 설정한다.
+        tPicker.setVisibility(View.INVISIBLE);
+        calView.setVisibility(View.INVISIBLE);
+
+        rdoCal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tPicker.setVisibility(View.INVISIBLE);
+                calView.setVisibility(View.VISIBLE);
+            }
+        });
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tPicker.setVisibility(View.VISIBLE);
+                calView.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        // 타이머 설정
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime());
+                chrono.start();
+                chrono.setTextColor(Color.RED);
+            }
+        });
+        // 버튼을 클릭하면 날짜, 시간을 가져온다.
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+
+                tvYear.setText(Integer.toString(selectYear));
+                tvMonth.setText(Integer.toString(selectMonth));
+                tvDay.setText(Integer.toString(selectDay));
+                tvHour.setText(Integer.toString(tPicker.getCurrentHour()));
+                tvMinute.setText(Integer.toString(tPicker.getCurrentMinute()));
+            }
+        });
+
+        calView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                selectYear = year;
+                selectMonth = month + 1; // 0부터 시작하기 때문에 1을 추가했음음
+                selectDay = dayOfMonth;
+            }
+        });
     }
 }
