be047c9269e64b5028b5f2c1ebadaa332beece93
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-10-26 02:06:28 +0900

    1026

--- ch7/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
+++ ch7/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
@@ -3,9 +3,11 @@ package kr.ac.hs.and2021.myapplication;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
                 dlg.setTitle("제목입니다");
                 dlg.setMessage("이곳이 내용입니다.");
                 dlg.setIcon(R.mipmap.ic_launcher);
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "확인을 눌렀네요", Toast.LENGTH_SHORT).show();
+                    }
+                });
                 dlg.show();
             }
         });
