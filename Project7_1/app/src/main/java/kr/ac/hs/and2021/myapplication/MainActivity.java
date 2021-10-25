f86ee85263fb9dc10ffb659c40b0a0caa2561f64
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-10-26 02:32:48 +0900

    1026

---
+++ Project7_1/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package kr.ac.hs.and2021.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout baseLayout;
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("배경색 바꾸기");
+        baseLayout = (LinearLayout) findViewById(R.id.baseLayout);
+        button1 = (Button) findViewById(R.id.button1);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
