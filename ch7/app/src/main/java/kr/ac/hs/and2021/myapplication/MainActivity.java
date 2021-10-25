78fadae8c3bece602522990dac891c25409424b2
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-10-26 01:46:33 +0900

    1026

---
+++ ch7/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package kr.ac.hs.and2021.myapplication;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setTitle("제목입니다");
+                dlg.setMessage("이곳이 내용입니다.");
+                dlg.setIcon(R.mipmap.ic_launcher);
+                dlg.show();
+            }
+        });
+    }
+}
