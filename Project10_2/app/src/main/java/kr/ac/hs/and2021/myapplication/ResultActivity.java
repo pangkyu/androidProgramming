c385d4f6250c6947ad2905bad120685cf487bbf0
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-11-11 22:37:30 +0900

    1111

--- Project10_2/app/src/main/java/kr/ac/hs/and2021/myapplication/ResultActivity.java
+++ Project10_2/app/src/main/java/kr/ac/hs/and2021/myapplication/ResultActivity.java
@@ -3,6 +3,10 @@ package kr.ac.hs.and2021.myapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -16,5 +20,26 @@ public class ResultActivity extends Activity {
         Intent intent = getIntent();
         int[] voteResult = intent.getIntArrayExtra("VoteCount");
         String[] imageName = intent.getStringArrayExtra("ImageName");
+        TextView tv[] = new TextView[imageName.length];
+        RatingBar rbar[] = new RatingBar[imageName.length];
+
+        Integer tvID[] = {R.id.tv1,R.id.tv2,R.id.tv3,R.id.tv4,R.id.tv5,R.id.tv6,R.id.tv7,R.id.tv8,R.id.tv9};
+        Integer rbarID[] = {R.id.rbar1,R.id.rbar2,R.id.rbar3,R.id.rbar4,R.id.rbar5,R.id.rbar6,R.id.rbar7,R.id.rbar8,R.id.rbar9};
+        for ( int i = 0; i < voteResult.length; i++){
+            tv[i] = (TextView) findViewById(tvID[i]);
+            rbar[i] = (RatingBar) findViewById(rbarID[i]);
+        }
+        for ( int i = 0; i< voteResult.length; i++){
+            tv[i].setText(imageName[i]);
+            rbar[i].setRating((float) voteResult[i]);
+        }
+
+        Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
