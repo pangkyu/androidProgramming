328c7489bc4b4c4b770dff20c10e51630f74c5ec
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-11-16 20:10:34 +0900

    1116

--- Project10_3/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
+++ Project10_3/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
@@ -2,13 +2,49 @@ package kr.ac.hs.and2021.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("액티비티 테스트 예제 ");
+        android.util.Log.i("액티비티 테스트", "onCreate()");
+
+        Button btnDial = (Button) findViewById(R.id.btnDial);
+        btnDial.setOnClickListener(v -> {
+            Uri uri = Uri.parse("tel : 010-1234-7899");
+            Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+            startActivity(intent);
+        });
+        Button btnFinish = (Button) findViewById(R.id.btnFinish);
+        btnFinish.setOnClickListener(v -> finish());
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        android.util.Log.i("액티비티 테스트", "onResume()");
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        android.util.Log.i("액티비티 테스트", "onStart()");
+    }
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        android.util.Log.i("액티비티 테스트", "onRestart()");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        android.util.Log.i("액티비티 테스트", "onPause()");
     }
 }
