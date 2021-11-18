b266ff2ea53ffe8a02c548d40528b80f39fd5885
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-11-18 19:46:19 +0900

    1118

--- Project12_1/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
+++ Project12_1/app/src/main/java/kr/ac/hs/and2021/myapplication/MainActivity.java
@@ -2,13 +2,93 @@ package kr.ac.hs.and2021.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    myDBHelper myHelper;
+    EditText edtName, edtNumber, edtNameResult, edtNumberResult;
+    Button btnInit, btnInsert, btnSelect;
+    SQLiteDatabase sqlDB;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        setTitle("가수 그룹 관리 db");
+
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtNumber = (EditText) findViewById(R.id.edtNumber);
+        edtNameResult = (EditText) findViewById(R.id.edtNameResult);
+        edtNumberResult = (EditText) findViewById(R.id.edtNumberResult);
+
+        btnInit = (Button) findViewById(R.id.btnInit);
+        btnInsert = (Button) findViewById(R.id.btnInsert);
+        btnSelect = (Button) findViewById(R.id.btnSelect);
+
+        myHelper = new myDBHelper(this);
+        btnInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqlDB, 1, 2); // 인수는 아무거나 입력하면 됨
+                sqlDB.close();
+            }
+        });
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDB = myHelper.getWritableDatabase();
+                sqlDB.execSQL("insert into groupTBL values ('"
+                + edtName.getText().toString() + "', "
+                        + edtNumber.getText().toString() + ");");
+                sqlDB.close();
+                Toast.makeText(getApplicationContext(), "입력됨", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnSelect.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                sqlDB = myHelper.getReadableDatabase();
+                Cursor cursor;
+                cursor = sqlDB.rawQuery("select * from groupTBL;", null);
+
+                String strNames = "그룹이름" + "\r\n" + "------" + "\r\n";
+                String strNumbers = "인원" + "\r\n" + "------" + "\r\n";
+
+                while (cursor.moveToNext()){
+                    strNames += cursor.getString(0)+"\r\n";
+                    strNumbers += cursor.getString(1) + "\r\n";
+                }
+                edtNameResult.setText(strNames);
+                edtNumberResult.setText(strNumbers);
+
+                cursor.close();
+                sqlDB.close();
+            }
+        });
+
+
+    }
+    public class myDBHelper extends SQLiteOpenHelper{
+        public myDBHelper(Context context) {
+            super(context, "groupDB", null, 1);
+        }
+        @Override
+        public void onCreate(SQLiteDatabase db){
+            db.execSQL("CREATE TABLE groupTBL ( gName char(20) primary key, gNumber integer);");
+        }
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+            db.execSQL("DROP TABLE IF EXISTS groupTBL");
+            onCreate(db);
+        }
+
     }
 }
