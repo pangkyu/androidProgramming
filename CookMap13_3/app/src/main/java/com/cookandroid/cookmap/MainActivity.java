f475005c102254957b7e13b1dd474b808ad986ff
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2021-11-28 18:31:28 +0900

    1128

--- CookMap13_3/app/src/main/java/com/cookandroid/cookmap/MainActivity.java
+++ CookMap13_3/app/src/main/java/com/cookandroid/cookmap/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -42,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onMapClick(LatLng point) {
                 videoMark = new GroundOverlayOptions().image(
                         BitmapDescriptorFactory
-                                .fromResource(R.drawable.presence_video_busy))
+                                .fromResource(R.drawable.rate_star_small_on))
                         .position(point, 100f, 100f);
                 gMap.addGroundOverlay(videoMark);
             }
@@ -55,6 +56,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         menu.add(0, 1, 0, "위성 지도");
         menu.add(0, 2, 0, "일반 지도");
         menu.add(0, 3, 0, "월드컵경기장 바로가기");
+        SubMenu fmspot = menu.addSubMenu("유명장소 바로가기 >> ");
+        fmspot.add(0,4,0,"한신대학교 60주년기념관");
+        fmspot.add(0,5,0,"병점역");
+        fmspot.add(0,6,0,"체이스센터");
+
+
         return true;
     }
 
@@ -70,6 +77,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             case 3:
                 gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
                         37.568256, 126.897240), 15));
+                return true;
+            case 4:
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                        37.19259, 127.02743), 15));
+                return true;
+            case 5:
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                        37.20732, 127.03317), 15));
+                return true;
+            case 6:
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                        37.77025, -122.38800), 15));
                 return true;
         }
         return false;
